b715889bf08d60498b6f6682bece0961b1e8e388
stefanstifter <stefan.stifter@saswebdev.com>
2019-10-24 01:43:09 +0200

    apply some styling

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <counter>the counter!</counter>
+    <counter>the counter</counter>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,21 +1,15 @@
 <template>
-  <main class="px-12 py-4 h-screen bg-gray-700">
-    <header>
-      <h1 class>
+  <main class="p-4 h-screen bg-gray-700">
+    <header class="text-center leading-none">
+      <h1 class="text-4xl text-gray-400 font-mono font-semibold tracking-wide">
         <slot></slot>
       </h1>
-      <p>increase the counter, as you like:</p>
+      <p class="text-gray-800">increase the counter, as you like</p>
     </header>
-    <section class="max-w-xs flex justify-between items-center">
-      <div class="w-12 h-16 border rounded-sm font-xl text-center">
-        <button @click="value1 += 1" class="block h-full w-full" type="button">{{ value1 }}</button>
-      </div>
-      <div class="w-12 h-16 border rounded-sm font-xl text-center">
-        <button @click="value2 += 1" class="block h-full w-full" type="button">{{ value2 }}</button>
-      </div>
-      <div class="w-12 h-16 border rounded-sm font-xl text-center">
-        <button @click="value3 += 1" class="block h-full w-full" type="button">{{ value3 }}</button>
-      </div>
+    <section class="max-w-xxs mx-auto mt-8 flex justify-between items-center text-3xl text-gray-400">
+      <button @click="value1 += 1" type="button" class="block w-16 h-24 text-center rounded-sm border border-gray-800 hover:border-gray-400 focus:outline-none focus:border-gray-400">{{ value1 }}</button>
+      <button @click="value2 += 1" type="button" class="block w-16 h-24 text-center rounded-sm border border-gray-800 hover:border-gray-400 focus:outline-none focus:border-gray-400">{{ value2 }}</button>
+      <button @click="value3 += 1" type="button" class="block w-16 h-24 text-center rounded-sm border border-gray-800 hover:border-gray-400 focus:outline-none focus:border-gray-400">{{ value3 }}</button>
     </section>
   </main>
 </template>
@@ -31,3 +25,10 @@ export default {
   }
 };
 </script>
+
+<style>
+  .max-w-xxs {
+    max-width: 16rem;
+  }
+
+</style>
